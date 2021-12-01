e933ee0a22149f7cd74d633f2c91a6f4a76f31e7
Thyago Nunes <thyagonunes43@gmail.com>
2021-12-01 20:47:35 -0300

    Objendo um usuário por id

--- src/main/java/br/com/fujioka/workshopmongo/services/UserService.java
+++ src/main/java/br/com/fujioka/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package br.com.fujioka.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fujioka.workshopmongo.domain.User;
+import br.com.fujioka.workshopmongo.exception.ObjectNotFoundException;
 import br.com.fujioka.workshopmongo.repositories.UserRepository;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository repo;
 	
 	public List<User> findAll(){
-		return repository.findAll();
+		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
