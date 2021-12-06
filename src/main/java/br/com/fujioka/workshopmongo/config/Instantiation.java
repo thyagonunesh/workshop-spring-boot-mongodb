ce52cfa159993124aa07c2a93a1aee3a36012072
Thyago Nunes <thyagonunes43@gmail.com>
2021-12-06 15:11:22 -0300

    Referenciando os posts do usuário e endpoint para posts

--- src/main/java/br/com/fujioka/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/fujioka/workshopmongo/config/Instantiation.java
@@ -43,6 +43,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		postReporitory.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 
 }
--- src/main/java/br/com/fujioka/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/fujioka/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.fujioka.workshopmongo.domain.Post;
 import br.com.fujioka.workshopmongo.domain.User;
 import br.com.fujioka.workshopmongo.dto.UserDTO;
 import br.com.fujioka.workshopmongo.services.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
