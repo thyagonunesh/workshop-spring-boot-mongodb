0019e7637192c201f670b70888f83278f8d1bbb0
Thyago Nunes <thyagonunes43@gmail.com>
2021-12-06 14:50:42 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/br/com/fujioka/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/fujioka/workshopmongo/config/Instantiation.java
@@ -1,7 +1,6 @@
 package br.com.fujioka.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import br.com.fujioka.workshopmongo.domain.Post;
 import br.com.fujioka.workshopmongo.domain.User;
+import br.com.fujioka.workshopmongo.dto.AuthorDTO;
 import br.com.fujioka.workshopmongo.repositories.PostRepository;
 import br.com.fujioka.workshopmongo.repositories.UserRepository;
 
@@ -36,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postReporitory.saveAll(Arrays.asList(post1, post2));
 		
 	}
